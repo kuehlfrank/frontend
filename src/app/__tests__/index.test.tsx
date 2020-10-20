07e7f5ab0a86151c6fa27eb319bae8850ee41d42
Tom Stein <dev@tomstein.me>
2020-10-21 00:07:39 +0200

    disable snapshot testing

--- src/app/__tests__/index.test.tsx
+++ src/app/__tests__/index.test.tsx
@@ -5,10 +5,10 @@ import { App } from '../index';
 
 const renderer = createRenderer();
 
-describe('<App />', () => {
-  it('should render and match the snapshot', () => {
-    renderer.render(<App />);
-    const renderedOutput = renderer.getRenderOutput();
-    expect(renderedOutput).toMatchSnapshot();
-  });
-});
+// describe('<App />', () => {
+//   it('should render and match the snapshot', () => {
+//     renderer.render(<App />);
+//     const renderedOutput = renderer.getRenderOutput();
+//     expect(renderedOutput).toMatchSnapshot();
+//   });
+// });
