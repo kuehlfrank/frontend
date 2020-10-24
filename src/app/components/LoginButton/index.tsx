d3c933e97e92dd6dea7e817c1b4f5595fb6c582b
Nico Kranz <nico@kranz.space>
2020-10-24 04:01:30 +0200

    use redirect auth flow

--- src/app/components/LoginButton/index.tsx
+++ src/app/components/LoginButton/index.tsx
@@ -3,14 +3,14 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { Button } from 'react-bootstrap';
 
 export function LoginButton() {
-  const { loginWithPopup } = useAuth0();
+  const { loginWithRedirect } = useAuth0();
   return (
     <>
       <Button
         type="button"
         variant="primary"
         onClick={() =>
-          loginWithPopup({
+          loginWithRedirect({
             returnTo: window.location.origin,
           })
         }
