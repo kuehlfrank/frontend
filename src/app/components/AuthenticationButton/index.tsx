6eddd5822e2a852e03febdd81b7eab794b96779b
Nico Kranz <nico@kranz.space>
2020-10-30 22:59:25 +0100

    removed signup button

--- src/app/components/AuthenticationButton/index.tsx
+++ src/app/components/AuthenticationButton/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { LoginButton } from '../LoginButton';
 import { LogoutButton } from '../LogoutButton';
-import { SignupButton } from '../SignupButton';
 
 export function AuthenticationButton() {
   const { isAuthenticated } = useAuth0();
@@ -12,7 +11,6 @@ export function AuthenticationButton() {
   ) : (
     <>
       <LoginButton />
-      <SignupButton />
     </>
   );
 }
